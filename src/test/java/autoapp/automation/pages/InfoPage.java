e11f406229844e0dac3074f21a211446331553eb
pradeepramidi <pradeep.ramidi@team.telstra.com>
2019-03-19 09:44:16 +1100

    info

---
+++ src/test/java/autoapp/automation/pages/InfoPage.java
@@ -0,0 +1,15 @@
+package autoapp.automation.pages;
+
+import autoapp.automation.utility.BrowserDriver;
+import cucumber.api.DataTable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.Map;
+
+public class InfoPage extends BasePage{
+
+
+    }
+}
